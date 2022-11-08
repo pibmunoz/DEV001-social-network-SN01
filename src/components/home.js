e7a91a610245b3c8309fdbf7937f0dedb0d84524
karlaCRM <abraxaskrm@gmail.com>
2022-11-08 08:25:36 -0600

    css de pagina home

--- src/components/home.js
+++ src/components/home.js
@@ -1,32 +1,40 @@
 export const home = () => {
     const homeDiv = document.createElement('div');
 
+    homeDiv.classList.add("cuerpo")
+
     const text = `  <section class="homeDivi">
     <div class="logo1">
       <img class= "logo" src="./img/pawsfinder.png" alt="Logo">
     </div>
   
     <div class="mainBox"> 
-        <img  class="iconoEmail" src="/img/iconoEmail_Mesa.png" alt= "icono email">
+        
         <input  type= "email" class="input1" placeholder="E-mail"> 
-        <img src= "" alt= "">
+        <img  class="iconoEmail" src="/img/email.png" alt= "icono email">
+       
         <input type="password" class="input1" placeholder="Password">
+        <img class="iconoPassword" src= "/img/password.png" alt= "password">
         <button class="buttonSignIn">Sign In</button>
-        <p><span class="text1">Doesn’t have an account yet? </span> <a class="text2" href="/register.js">Register here</a></p>
+        <p class="registerText"><span class="text1">Doesn’t have an account yet? </span> <a class="text2" href="/register.js">Register here</a></p>
         <div class="login2">
-          <p>Or login with</p>
-          <a href="">Google</a>
-          <a href="">Facebook</a>
+          <div class="acomodo">
+          <hr> <p>Or login with</p> <hr>
+          </div>
+          <div class="google">
+          <img class="iconoGoogle" src= "/img/google.svg" alt= "google">
+          <div class = "circle" id= "circle"></div>
+          </div>
+          
         </div> 
+        <img  class="imgFooter" src="./img/puppy1.png" alt="Puppy2">
     </div>
-    <footer class="footer1">
-    <img  class="imgFooter" src="./img/puppy_22.png" alt="Puppy2">
-  </footer>
+    
   </section>
  
    `
 
-    homeDiv.innerHTML= text
+    homeDiv.insertAdjacentHTML("beforeend", text)
 
     return homeDiv;
 
