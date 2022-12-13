f9592321782a08f821b77f35f90fec1375891182
Pilar Muñoz <86972128+pibmunoz@users.noreply.github.com>
2022-12-13 20:41:32 -0300

    Avance tests Co-authored-by: Karla Rodríguez marin <karlaCRM@users.noreply.github.com> Co-authored-by: gabrielavillarrealdiaz <gabrielavillarrealdiaz@users.noreply.github.com>

--- __mocks__/firebase/auth.js
+++ __mocks__/firebase/auth.js
@@ -11,17 +11,9 @@ export const signInWithPopup = jest.fn();
 export const provider = jest.fn(GoogleAuthProvider());
 export const updateProfile = jest.fn();
 export const signOut = jest.fn();
+export const submitRegister = jest.fn();
 // PENDIENTE POR REVISAR
 // eslint-disable-next-line max-len
-export const submitRegister = jest.fn((email, password, name, country) => new Promise((resolve, reject) => {
-  const userCredential = {
-    user: {
-      uid: '12345',
-    },
-  };
-  resolve(userCredential);
-  reject(new Error(AuthErrorCodes));
-}));
 
 /* jest.fn((auth, email, password) => {
   Promise.resolve({ user: { email, password } });
